2d2abad5c08288e33e6a64f6b257859fd07942f5
phuhh <huynhhieu.phu@gmail.com>
2023-05-04 22:20:13 +0700

    #41 done migrate, schema, seeder, factory

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'created_at' => new \DateTime(),
+            'updated_at' => new \DateTime(),
+        ];
+    }
+}
