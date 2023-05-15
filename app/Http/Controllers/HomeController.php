b9ce1a3d7f84e743eb63bb66fd47d4c4e770835a
phuhh <huynhhieu.phu@gmail.com>
2023-05-15 15:02:58 +0700

    #45 done auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function logInfo()
+    {
+        echo Auth::user()->id . '<br>';
+        echo Auth::id() . '<br>';
+        echo Auth::user()->name . '<br>';
+        echo Auth::user()->email . '<br>';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +13,20 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/', 'welcome');
+
+Auth::routes(['verify' => true]);
+
+Route::middleware(['password.confirm', 'verified'])
+    ->get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home');
 
 
+Route::get('/log-info', [HomeController::class, 'logInfo']);
+
+Route::get('/check-login', function (){
+    if (Auth::check()){
+        return 'da dang nhap';
+    }
+    return 'chua dang nhap';
+});
